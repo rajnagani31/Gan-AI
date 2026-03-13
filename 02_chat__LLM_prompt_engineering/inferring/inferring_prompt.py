98fe4f6c46780d29214c9f0026f1e22dd8b6733b
rajnagani31 <rajnagani331@gmail.com>
2026-03-13 18:39:04 +0530

    learn about inferring

---
+++ 02_chat__LLM_prompt_engineering/inferring/inferring_prompt.py
@@ -0,0 +1,116 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from IPython.display import display, HTML
+
+
+load_dotenv()
+
+client = OpenAI()
+
+def get_completion(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response.choices[0].message.content
+
+lamp_review = """
+Needed a nice lamp for my bedroom, and this one had \
+additional storage and not too high of a price point. \
+Got it fast.  The string to our lamp broke during the \
+transit and the company happily sent over a new one. \
+Came within a few days as well. It was easy to put \
+together.  I had a missing part, so I contacted their \
+support and they very quickly got me the missing piece! \
+Lumina seems to me to be a great company that cares \
+about their customers and products!!
+"""
+
+prompt = f"""
+Identify the following items from the review text: 
+- Sentiment (positive or negative)
+- Is the reviewer expressing anger? (true or false)
+- Item purchased by reviewer
+- Company that made the item
+
+The review is delimited with triple backticks. \
+Format your response as a JSON object with \
+"Sentiment", "Anger", "Item" and "Brand" as the keys.
+If the information isn't present, use "unknown" \
+as the value.
+Make your response as short as possible.
+Format the Anger value as a boolean.
+
+Review text: '''{lamp_review}'''
+"""
+
+
+story = """
+In a recent survey conducted by the government, 
+public sector employees were asked to rate their level 
+of satisfaction with the department they work at. 
+The results revealed that NASA was the most popular 
+department with a satisfaction rating of 95%.
+
+One NASA employee, John Smith, commented on the findings, 
+stating, "I'm not surprised that NASA came out on top. 
+It's a great place to work with amazing people and 
+incredible opportunities. I'm proud to be a part of 
+such an innovative organization."
+
+The results were also welcomed by NASA's management team, 
+with Director Tom Johnson stating, "We are thrilled to 
+hear that our employees are satisfied with their work at NASA. 
+We have a talented and dedicated team who work tirelessly 
+to achieve our goals, and it's fantastic to see that their 
+hard work is paying off."
+
+The survey also revealed that the 
+Social Security Administration had the lowest satisfaction 
+rating, with only 45% of employees indicating they were 
+satisfied with their job. The government has pledged to 
+address the concerns raised by employees in the survey and 
+work towards improving job satisfaction across all departments.
+"""
+
+prompt0 = f"""
+Determine five topics that are being discussed in the \
+following text, which is delimited by triple backticks.
+
+Make each item one or two words long. 
+
+Format your response as a list of items separated by commas.
+
+Text sample: '''{story}'''
+"""
+
+
+# response = get_completion(prompt)
+
+topic_list =['1. Survey', '2. Job satisfaction', '3. NASA', '4. Social Security Administration', '5. Government pledge']
+
+prompt1 = f"""
+Determine whether each item in the following list of \
+topics is a topic in the text below, which
+is delimited with triple backticks.
+
+Give your answer as follows:
+item from the list: 0 or 1
+
+List of topics: {", ".join(topic_list)}
+
+Text sample: '''{story}'''
+"""
+response = get_completion(prompt)
+print(response)
+
+
+{
+    "Sentiment": "positive",
+    "Anger": False,
+    "Item": "lamp",
+    "Brand": "Lumina"
+}
