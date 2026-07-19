4a179e6dcf4946b52b9f24a9cdba0c33dba68003
rajnagani31 <rajnagani331@gmail.com>
2026-07-19 11:58:26 +0530

    learn: groq llm with free tire

---
+++ 14_groq/main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "What is fastapi?",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+    stream= True
+)
+
+# print(chat_completion.choices[0].message.content)
+
+for chunk in chat_completion:
+    print(chunk.choices[0].delta.content, end ="")
