e311745c574d70c86fe1e0007cc374dce50e2584
rajnagani31 <rajnagani331@gmail.com>
2026-07-29 23:33:01 +0530

    fix

--- 14_groq/main.py
+++ 14_groq/main.py
@@ -7,7 +7,11 @@ load_dotenv()
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
-
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
 
 chat_completion = client.chat.completions.create(
     messages=[
